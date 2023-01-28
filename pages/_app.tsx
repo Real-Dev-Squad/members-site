a2658e9326794150d870ce85198ab628776bfb55
Samyak Shah <samyakshah3008@gmail.com>
2023-01-28 09:44:36 +0530

    FEAT: redux toolkit setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "@/src/store";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return  <Provider store = {store}> <Component {...pageProps} /> </Provider> 
 }
