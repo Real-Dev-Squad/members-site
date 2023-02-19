c0b15516429067835fe00ff24128d4b4ad080564
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-02-19 18:35:03 +0530

    change folder structure, make variable for navbarmenu, import image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import Layout from './components/Navbar/Layout'
+import Layout from '../components/pages/Navbar/Layout'
 
 export default function Index() {
   return (
