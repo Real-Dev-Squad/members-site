ab4cc1664f925bd8267dbcc9248677fe0a41a5fd
vinayak <trivedivinayak57@gmail.com>
2023-04-22 10:16:30 +0530

    added chakra UI provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
-import type { AppProps } from "next/app";
-import LayoutComponent from "@/components/Layout/LayoutComponent";
-import { Provider } from "react-redux";
-import { wrapper } from "@/src/store";
-import "@/styles/globals.css";
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import LayoutComponent from '@/components/Layout/LayoutComponent';
+import { Provider } from 'react-redux';
+import { wrapper } from '@/src/store';
+import '@/styles/globals.css';
 
 function App({ Component, ...rest }: AppProps) {
   // wrapping all the props with store wrapper
-  const {store, props} = wrapper.useWrappedStore(rest);
+  const { store, props } = wrapper.useWrappedStore(rest);
   return (
-    <Provider store={store}>
-      <LayoutComponent>
-        <Component {...props} />
-      </LayoutComponent>
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <LayoutComponent>
+          <Component {...props} />
+        </LayoutComponent>
+      </Provider>
+    </ChakraProvider>
   );
 }
 
