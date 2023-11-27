a3dddc381e4ef0f6c4f06c0d29a66e300be7a66b
Anish Pawaskar <anishpawaskar@gmail.com>
2023-11-28 02:19:44 +0530

    refactor auth authHandler

--- src/components/UtilComponents/AuthHandler.tsx
+++ src/components/UtilComponents/AuthHandler.tsx
@@ -1,24 +1,28 @@
 import { useGetSelfDetailsQuery } from "@/src/services/serverApi";
 import { setIsLoggedIn, setUserData } from "@/src/store/global";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 type Props = {
   children: JSX.Element;
 };
 
-
 export default function AuthHandler(props: Props) {
-  const {data: user, isLoading} = useGetSelfDetailsQuery();
+  const { data: user, isLoading } = useGetSelfDetailsQuery();
   const reduxDispatch = useDispatch();
 
-  if (!isLoading && user) {
-    reduxDispatch(setIsLoggedIn({ isLoggedIn: true }));
-    reduxDispatch(setUserData({
-      first_name: user?.first_name,
-      imageURL: user?.picture?.url,
-      roles: user?.roles,
-    }));
-  }
+  useEffect(() => {
+    if (!isLoading && user) {
+      reduxDispatch(setIsLoggedIn({ isLoggedIn: true }));
+      reduxDispatch(
+        setUserData({
+          first_name: user?.first_name,
+          imageURL: user?.picture?.url,
+          roles: user?.roles,
+        })
+      );
+    }
+  }, [isLoading, user, reduxDispatch]);
 
   return props.children;
 }
