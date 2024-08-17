8a363c61267a7c5ef7edbf973d7fca920f3212cc
yash raj <yashraj0207@outlook.com>
2024-08-17 15:12:04 +0530

    return maintenance page from root route

---
+++ src/components/MaintenancePage/MaintenancePage.tsx
@@ -0,0 +1,10 @@
+import styles from "./MaintenancePage.module.css"
+
+export const MaintenancePage = () => {
+  return (
+    <div className={styles.maintenancePageContainer}>
+      <h1>Site is under maintenance</h1>
+      <p>Sorry for the inconvenience. Please come back later.</p>
+    </div>
+  )
+}
