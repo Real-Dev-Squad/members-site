79b1e16ad6e9e511be47bf896970c323762db8b6
yash raj <yashraj0207@outlook.com>
2024-08-18 02:44:44 +0530

    remove feature flag from homepage and delete maintenance page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,8 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { wrapper } from '@/src/store';
-import serverApi, { useGetAllUsersQuery } from '../src/services/serverApi';
-// import NewMembersCard from "@/src/components/NewMember";
-import { NEW_USER, NUM_MEMBERS_NUMBER } from '@/src/constants/AppConstants';
-import styles from '@/styles/Home.module.css';
 import MembersSectionMain from '@/src/components/MembersSectionNew/MembersSectionMain';
 import NewMemberSection from '@/src/components/NewMemberSection';
+import { NEW_USER } from '@/src/constants/AppConstants';
+import styles from '@/styles/Home.module.css';
 import { UserType } from '../src/components/MembersSectionNew/types/MembersSection.type';
-import { MaintenancePage } from '@/src/components/MaintenancePage/MaintenancePage';
-import { useRouter } from 'next/router';
 type PictureType = {
   publicId: string;
   url: string;
@@ -34,25 +27,16 @@ type PropsType = {
 };
 
 export default function Home() {
-  const { query } = useRouter()
-
-  if (query.dev === "true") {
-    return (
-      <div className={styles.container}>
-        <div>
-          <h1 className={styles.heading}>Real Dev Squad Members</h1>
-          <MembersSectionMain />
-        </div>
-        <div>
-          <h1 className={styles.heading}>{NEW_USER}</h1>
-          <NewMemberSection />
-        </div>
-      </div>
-    );
-  }
-
-  // TODO: Remove this once the /users api has support for `role` query param
   return (
-    <MaintenancePage />
-  )
+    <div className={styles.container}>
+      <div>
+        <h1 className={styles.heading}>Real Dev Squad Members</h1>
+        <MembersSectionMain />
+      </div>
+      <div>
+        <h1 className={styles.heading}>{NEW_USER}</h1>
+        <NewMemberSection />
+      </div>
+    </div>
+  );
 }
