2d3210804968db315d883dec067549ca75b57c62
yash raj <yashraj0207@outlook.com>
2024-08-17 15:21:22 +0530

    update message

--- src/components/MaintenancePage/MaintenancePage.tsx
+++ src/components/MaintenancePage/MaintenancePage.tsx
@@ -3,8 +3,9 @@ import styles from "./MaintenancePage.module.css"
 export const MaintenancePage = () => {
   return (
     <div className={styles.maintenancePageContainer}>
-      <h1>Site is under maintenance</h1>
-      <p>Sorry for the inconvenience. Please come back later.</p>
+      <h1>Page is under maintenance</h1>
+      <p>We apologize for the inconvenience. Our page is currently undergoing maintenance.</p>
+      <p>Please check back later. Thank you for your patience.</p>
     </div>
   )
 }
