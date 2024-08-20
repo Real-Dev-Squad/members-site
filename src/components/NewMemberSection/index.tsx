84c45999f7a97ff48fd57ffcd2b5ec85cc7ae241
Ashif Khan <ashifkhn02@gmail.com>
2024-08-20 17:27:32 +0530

    feat:added infinite laoding

--- src/components/NewMemberSection/index.tsx
+++ src/components/NewMemberSection/index.tsx
@@ -1,21 +1,48 @@
+import { useEffect, useRef } from 'react';
 import { useGetUserWithLoadMore } from '@/src/services/serverApi';
 import NewMemberSectionPresentation from './Presentation';
 import { Button } from '@chakra-ui/react';
-import styles from './newMemberSection.module.css'
+import styles from './newMemberSection.module.css';
 
 export default function NewMemberSection() {
-  const { data, isLoading, isFetching, loadMore, links } = useGetUserWithLoadMore()
+  const { data, isLoading, isFetching, loadMore, links } =
+    useGetUserWithLoadMore();
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!loadMoreRef.current) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && links?.next) {
+          loadMore();
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0,
+      },
+    );
+
+    observer.observe(loadMoreRef.current);
+
+    return () => {
+      if (loadMoreRef.current) {
+        observer.unobserve(loadMoreRef.current);
+      }
+    };
+  }, [loadMore, links]);
 
   return (
     <div>
-      <NewMemberSectionPresentation
-        data={data}
-        isLoading={isLoading}
-      />
+      <NewMemberSectionPresentation data={data} isLoading={isLoading} />
 
-      {links?.next && links.next.length &&
-        <div className={styles.newMemberSectionLoadMore__container}>
-
+      {links?.next && links.next.length && (
+        <div
+          className={styles.newMemberSectionLoadMore__container}
+          ref={loadMoreRef}
+        >
           <Button
             onClick={loadMore}
             isLoading={isFetching}
@@ -25,7 +52,7 @@ export default function NewMemberSection() {
             Load More
           </Button>
         </div>
-      }
+      )}
     </div>
   );
 }
