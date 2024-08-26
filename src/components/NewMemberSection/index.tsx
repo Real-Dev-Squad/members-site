98ff656141d7389fb500622127fc993b8e92ba5d
Ashif Khan <54736284+ashifkhn@users.noreply.github.com>
2024-08-27 00:18:10 +0530

    feat:added infinite laoding (#160)
    * feat:added infinite laoding
    
    * feat: spinner added

--- src/components/NewMemberSection/index.tsx
+++ src/components/NewMemberSection/index.tsx
@@ -1,31 +1,51 @@
+import { useEffect, useRef } from 'react';
 import { useGetUserWithLoadMore } from '@/src/services/serverApi';
 import NewMemberSectionPresentation from './Presentation';
-import { Button } from '@chakra-ui/react';
-import styles from './newMemberSection.module.css'
+import { Button, Spinner } from '@chakra-ui/react';
+import styles from './newMemberSection.module.css';
 
 export default function NewMemberSection() {
-  const { data, isLoading, isFetching, loadMore, links } = useGetUserWithLoadMore()
+  const { data, isLoading, isFetching, loadMore, links } =
+    useGetUserWithLoadMore();
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!loadMoreRef.current) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && links?.next) {
+          loadMore();
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0,
+      },
+    );
+
+    observer.observe(loadMoreRef.current);
+
+    return () => {
+      if (loadMoreRef.current) {
+        observer.unobserve(loadMoreRef.current);
+      }
+    };
+  }, [loadMore, links]);
 
   return (
     <div>
-      <NewMemberSectionPresentation
-        data={data}
-        isLoading={isLoading}
-      />
+      <NewMemberSectionPresentation data={data} isLoading={isLoading} />
 
-      {links?.next && links.next.length &&
-        <div className={styles.newMemberSectionLoadMore__container}>
-
-          <Button
-            onClick={loadMore}
-            isLoading={isFetching}
-            loadingText="Loading..."
-            className={styles.newMemberSectionLoadMore__button}
-          >
-            Load More
-          </Button>
+      {links?.next && links.next.length && (
+        <div
+          className={styles.newMemberSectionLoadMore__container}
+          ref={loadMoreRef}
+        >
+          <Spinner/>
         </div>
-      }
+      )}
     </div>
   );
 }
