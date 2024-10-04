ce0ab4ec7d91b2037e0976d01db8e05f72340774
Ashif Khan <54736284+ashifkhn@users.noreply.github.com>
2024-10-04 10:20:21 +0530

    update:test fix (#170)
    Fixed test setup

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,9 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
