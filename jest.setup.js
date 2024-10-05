3008b2b4c5483b8c6c7eae929a107180354615fa
Achintya Chatterjee <55826451+Achintya-Chatterjee@users.noreply.github.com>
2024-10-05 15:14:23 +0530

    Dev to Main Sync (#180)
    Fixed test setup
    
    Co-authored-by: Ashif Khan <54736284+ashifkhn@users.noreply.github.com>

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,2 @@
 import '@testing-library/jest-dom';
-import './setupTests';
 import 'whatwg-fetch';
-
-// jest.config.js
-module.exports = {
-  setupFilesAfterEnv: ['@testing-library/jest-dom', './jest.setup.ts'],
-};
