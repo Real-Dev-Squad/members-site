4ee579e9d25f3e1acdd53af4a063f55ad1549660
Hariom Vashista <rishivashista4@gmail.com>
2024-12-08 05:23:54 +0530

    Update users/self API endpoint to the new /users?profile=true API endpoint

--- src/mocks/handlers/self.handler.ts
+++ src/mocks/handlers/self.handler.ts
@@ -3,7 +3,7 @@ import { selfUser } from '../db/user';
 const URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const selfUserHandler = [
-  rest.get(`${URL}/users/self`, (_, res, ctx) => {
+  rest.get(`${URL}/users?profile=true`, (_, res, ctx) => {
     return res(ctx.status(200), ctx.json(selfUser));
   }),
 ];
